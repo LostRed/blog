4fb34a5b6d967d542d6fbde77a7899ac6d97761a
Luwei Deng <lostred@outlook.com>
2021-03-03 23:40:44 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         port: 10000,
         proxy: {
             '/api': {
-                // target: 'http://lostred.info:8080',
+                // target: 'http://lostred.info:8000',
                 target: 'http://localhost:8080',
                 // 允许跨域
                 changeOrigin: true,
